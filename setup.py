3fce437f384894c085fe154d4758cb186b2acc8b
alvaromc317 <alvaromc317@hotmail.com>
2022-04-21 12:20:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='fpqr',
-    version='0.0.1',
+    packages=find_packages(),
+    version='0.0.2',
     author='Alvaro Mendez Civieta',
     author_email='alvaro.mendez@uc3m.es',
     license='GNU General Public License',
@@ -18,13 +19,12 @@ setup(
     description='A quantile based dimension reduction technique for regression',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    download_url='https://github.com/alvaromc317/fpqr/archive/refs/tags/v_0_0_1.tar.gz',
+    download_url='https://github.com/alvaromc317/fpqr/archive/refs/tags/v_0_0_2.tar.gz',
     keywords=['partial-least-squares', 'quantile-regression', 'dimension-reduction', 'outliers', 'robust'],
     python_requires='>=3.5',
     install_requires=["numpy >= 1.2",
                       "scipy >= 1.7.0",
                       "scikit-learn >= 1.0",
                       "asgl >= 1.0.5",
-                      "cvxpy >= 1.2.0"],
-    packages=find_packages()
+                      "cvxpy >= 1.2.0"]
 )
